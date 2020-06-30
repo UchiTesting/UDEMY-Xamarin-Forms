d21de29e6cc5418f8b445d7a80bb68f5bec6577d
UchiTesting <uchitesting57@gmail.com>
2020-06-30 21:37:15 +0200

    Finished exercice 1.

--- Exercice1/Exercice1/MainPage.xaml.cs
+++ Exercice1/Exercice1/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,44 @@ namespace Exercice1
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        public int FontSize { get; set; }
+        public List<string> Quotes { get; set; }
+        public int CurrentQuoteIndex { get; set; }
         public MainPage()
         {
             InitializeComponent();
+
+            Quotes = new List<string>()
+            {
+                "To be or not to be. That is the question.",
+                "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+                "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+                "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+                "Try not to become a man of success. Rather become a man of value."
+            };
+            CurrentQuoteIndex = 0;
+
+            DisplayQuote();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (CurrentQuoteIndex + 1 < Quotes.Count)
+                CurrentQuoteIndex++;
+            else
+                CurrentQuoteIndex = 0;
+
+            DisplayQuote();
+
+        }
+
+        private void DisplayQuote()
+        {
+            if (Quotes.Count > 0 && CurrentQuoteIndex < Quotes.Count)
+            {
+                lblQuotes.Text = Quotes[CurrentQuoteIndex];
+            }
+
         }
     }
 }
