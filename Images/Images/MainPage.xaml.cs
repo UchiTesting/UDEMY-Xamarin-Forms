ec3311576440d35cfea860c12d51429bc2b0f253
UchiTesting <uchitesting57@gmail.com>
2020-07-01 15:06:55 +0200

    Added a new project to put the code from the course about images.

---
+++ Images/Images/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Images
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            string uriString = "https://lorempixel.com/1920/1080/sports/7";
+
+            // Noisy code
+            //var imageSource = (UriImageSource)ImageSource.FromUri(new Uri(uriString));
+
+            // Better code.
+            var imageSource = new UriImageSource { Uri = new Uri(uriString) };
+            imageSource.CachingEnabled = false;
+            imageSource.CacheValidity = TimeSpan.FromDays(7.0);
+
+            image.Source = imageSource;
+            //image.Source = uriString; // Implicit conversion
+        }
+    }
+}
