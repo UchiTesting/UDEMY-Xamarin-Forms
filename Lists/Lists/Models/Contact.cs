3f15acc972ae665e24b68e562df6366d55915d64
UchiTesting <uchitesting57@gmail.com>
2020-07-02 20:47:56 +0200

    First version of list course code.

---
+++ Lists/Lists/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lists.Models
+{
+    class Contact
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
---
+++ Lists/Lists/Views/Basics.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Lists.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lists.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Basics : ContentPage
+    {
+        public Basics()
+        {
+            InitializeComponent();
+
+            var contacts = new List<Contact>
+            {
+                new Contact{Name= "Maya", PhotoUrl="https://lorempixel.com/100/100/cats/1"},
+                new Contact{Name= "Cerise", PhotoUrl="https://lorempixel.com/100/100/cats/2", Status="I wantz my treatz!"},
+                new Contact{Name= "Liam", PhotoUrl="https://lorempixel.com/100/100/cats/3"}
+            };
+
+            listView.ItemsSource = contacts;
+        }
+    }
+}
