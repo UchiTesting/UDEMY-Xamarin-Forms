ccbb2b9678f55a2829309aff7b9792b564f15610
UchiTesting <uchitesting57@gmail.com>
2020-07-04 23:59:07 +0200

    Made layout exercices for AbsoluteLayout and Grid layout. Moved exercises in one unique project.

---
+++ LayoutsEx/LayoutsEx/Views/StackLayoutEx2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutsEx.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StackLayoutEx2 : ContentPage
+    {
+        public StackLayoutEx2()
+        {
+            InitializeComponent();
+        }
+    }
+}
