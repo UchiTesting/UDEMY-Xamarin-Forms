47c3bb0808dc7552ca2df7e511f0edcf57db0ca0
UchiTesing <uchitesting57@gmail.com>
2020-11-06 20:59:15 +0100

    Navigation Toolbar demo code.

---
+++ ToolbarDemo/ToolbarDemo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+using ToolbarDemo.Views;
+
+using Xamarin.Forms;
+
+namespace ToolbarDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // Toolbars needs a navigation page to display
+            MainPage = new NavigationPage(new MainPage());
+            // Change to this code is you want to observe it by yourself.
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ToolbarDemo/ToolbarDemo/Views/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+using Xamarin.Forms;
+
+namespace ToolbarDemo.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void New_Clicked(object sender, System.EventArgs e)
+        {
+            await DisplayAlert("New item", "New Item", "OK");
+        }
+
+        private async void Edit_Clicked(object sender, System.EventArgs e)
+        {
+            await DisplayAlert("Edit Item", "Edit item", "OK");
+        }
+
+        private async void Delete_Clicked(object sender, System.EventArgs e)
+        {
+            await DisplayAlert("Delete Item", "Delete item", "OK");
+        }
+    }
+}
