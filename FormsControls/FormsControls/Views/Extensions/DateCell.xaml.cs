80fe0d6a6bcde4f3de46b245de4b678e9ee6b106
UchiTesing <uchitesting57@gmail.com>
2020-11-09 22:33:08 +0100

    Added TableView Demo code and custom elements with custom properties. Exciting to say the least.

---
+++ FormsControls/FormsControls/Views/Extensions/DateCell.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormsControls.Views.Extensions
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DateCell : ViewCell
+    {
+        public static readonly BindableProperty LabelProperty =
+            BindableProperty.Create("Label", typeof(string), typeof(DateCell));
+
+        public static readonly BindableProperty DateProperty =
+            BindableProperty.Create("Date", typeof(DateTime), typeof(DateCell), DateTime.Today);
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public DateTime Date
+        {
+            get { return (DateTime)GetValue(DateProperty); }
+            set { SetValue(DateProperty, value); }
+        }
+        public DateCell()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
---
+++ FormsControls/FormsControls/Views/TableViewDemo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormsControls.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TableViewDemo : ContentPage
+    {
+        public TableViewDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void EntryCell_Completed(object sender, EventArgs e)
+        {
+            DisplayAlert("Entry completed", "Hello World", "OK");
+        }
+
+        private void SwitchCell_OnChanged(object sender, ToggledEventArgs e)
+        {
+            //DisplayAlert("Switch moved", "...", "KO");
+        }
+    }
+}
